86fde07463b08b81c90c81fdd823f89d226bfcec
mraiyuu <munenedenis618@gmail.com>
2025-03-16 11:24:27 +0300

    feat: db connection, docker compose

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/mraiyuu/social/internal/db"
 	"github.com/mraiyuu/social/internal/env"
 	"github.com/mraiyuu/social/internal/store"
 )
@@ -10,13 +11,30 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", "8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15min"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
-		store: store,
+		store:  store,
 	}
 
 	mux := app.mount()
