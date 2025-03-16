32c5f7b46f920e2a724762d8067b04a2b488cb06
mraiyuu <munenedenis618@gmail.com>
2025-03-16 09:56:45 +0300

    feat: save post on postgre database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,22 @@
 package main
 
-import "log"
+import (
+	"log"
 
+	"github.com/mraiyuu/social/internal/env"
+	"github.com/mraiyuu/social/internal/store"
+)
 
 func main() {
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR", "8080"),
 	}
+
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config: cfg,
+		store: store,
 	}
 
 	mux := app.mount()
